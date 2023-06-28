9b7c56719746b3bb1256128434a1eef38ee7e059
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-28 13:51:30 +0700

    copied tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        "whatsapp-500": "#32d951",
+        "whatsapp-600": "#1c9732",
+      },
+    },
+  },
+  plugins: [],
 }
